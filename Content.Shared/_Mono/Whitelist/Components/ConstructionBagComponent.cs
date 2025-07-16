9c3c28dc95f511f167ad1d1eead644a5c7801170
SRV <220771817+SarahRaven@users.noreply.github.com>
2025-07-16 19:11:55 +0000

    Engineer Cyborg Matching Salvage Cyborg for Bag Mechanic, Plus Slots for Plastic and Plasteel to Construct More (#1413)
    * Increases borg module maximums by two to allow each borg to easily accommodate the jetpack and GPS modules without hindering their normal module counts
    
    * Medical borgs can now pickup and drop organ for surgery. Xop Glorp 'Surgery'
    
    * Forgot about the alternate versions of borgs chassis max modules
    
    * Forgot to add in BodyPart to the Organ list too. Make sure they can reattach your limbs after you blow up
    
    * Made the construction bag function similar on engineer cyborg as ore bag on salvage cyborg
    
    * Modified engineer cyborg to have slots for plasteel, and more floor tiles
    
    * chore: automatically update REUSE headers
    
    * chore: automatically update REUSE headers
    
    * Modified to move namespace to _Mono for the change.
    
    ---------
    
    Co-authored-by: monolith8319 <195513600+monolith8319@users.noreply.github.com>

---
+++ Content.Shared/_Mono/Whitelist/Components/ConstructionBagComponent.cs
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: 2025 SarahRaven
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace Content.Shared._Mono.Whitelist.Components;
+
+/// <summary>
+/// Whitelist component for construction bags to avoid tag redefinition and collisions
+/// </summary>
+[RegisterComponent]
+public sealed partial class ConstructionBagComponent : Component;
