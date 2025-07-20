9fa807b67d221162f12e1b30276c7431f5e31f3b
core-mene <fluorine2@live.fr>
2025-07-20 08:48:07 +0200

    New Clothing Slots + Clothings for Them (#1396)
    * oh hell no
    
    * wtf
    
    * chore: automatically update REUSE headers
    
    * tests
    
    * less shit
    
    * AAAH
    
    * finally
    
    * guh
    
    * PROGRESS FOR ONCE
    
    * chore: automatically update REUSE headers
    
    * armbands n shit
    
    * done
    
    * it works, baller
    
    * DONE BALLER
    
    * baller
    
    * chore: automatically update REUSE headers
    
    * done-ish
    
    * moremoremore
    
    * rah
    
    * chore: automatically update REUSE headers
    
    * dope shit
    
    * GUESS SO
    
    * chore: automatically update REUSE headers
    
    * chore: automatically update REUSE headers
    
    * done???
    
    * DONE FINALLY
    
    * chore: automatically update REUSE headers
    
    * shit i forgot
    
    * fixed now
    
    * fixes again
    
    * chore: automatically update REUSE headers
    
    * end
    
    * chore: automatically update REUSE headers
    
    ---------
    
    Co-authored-by: monolith8319 <195513600+monolith8319@users.noreply.github.com>

--- Content.Shared/Inventory/SlotFlags.cs
+++ Content.Shared/Inventory/SlotFlags.cs
@@ -1,3 +1,14 @@
+// SPDX-FileCopyrightText: 2021 Paul Ritter
+// SPDX-FileCopyrightText: 2022 Leon Friedrich
+// SPDX-FileCopyrightText: 2022 ShadowCommander
+// SPDX-FileCopyrightText: 2022 ZeroDayDaemon
+// SPDX-FileCopyrightText: 2022 wrexbe
+// SPDX-FileCopyrightText: 2024 Plykiya
+// SPDX-FileCopyrightText: 2025 Ark
+// SPDX-FileCopyrightText: 2025 core-mene
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Robust.Shared.Serialization;
 
 namespace Content.Shared.Inventory;
@@ -27,6 +38,11 @@ public enum SlotFlags
     FEET = 1 << 14,
     SUITSTORAGE = 1 << 15,
     WALLET = 1 << 16, // Frontier: using an unused slot, redefine to a new bit if/when it's used (goodbye ushort)
+    BALACLAVA = 1 << 17, // Mono start
+    ARMBANDRIGHT = 1 << 18,
+    ARMBANDLEFT = 1 << 19,
+    HELMETCOVER = 1 << 20,
+    HELMETATTACHMENT = 1 << 21, //Mono end
     All = ~NONE,
 
     WITHOUT_POCKET = All & ~POCKET
