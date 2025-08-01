450c18cd9e7eb24e0082e1819433584fd549d7ab
tonotom1 <tonotom@proton.me>
2025-08-01 12:08:23 +1000

    Unstrangle Volumetrics (#1720)
    * unstrangle volumetrics
    
    * chore: automatically update REUSE headers
    
    ---------
    
    Co-authored-by: monolith8319 <195513600+monolith8319@users.noreply.github.com>

--- Content.Shared/CCVar/CCVars.Atmos.cs
+++ Content.Shared/CCVar/CCVars.Atmos.cs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2024 Simon
 // SPDX-FileCopyrightText: 2025 GreaseMonk
 // SPDX-FileCopyrightText: 2025 sleepyyapril
+// SPDX-FileCopyrightText: 2025 tonotom
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
@@ -141,7 +142,7 @@ public sealed partial class CCVars
     ///     in-game.
     /// </summary>
     public static readonly CVarDef<float> AtmosSpeedup =
-        CVarDef.Create("atmos.speedup", 2f, CVar.SERVERONLY); // Frontier 8f<2f
+        CVarDef.Create("atmos.speedup", 8f, CVar.SERVERONLY);
 
     /// <summary>
     ///     Like atmos.speedup, but only for gas and reaction heat values. 64x means
